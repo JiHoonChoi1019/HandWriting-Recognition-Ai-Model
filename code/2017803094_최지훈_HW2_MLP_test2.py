8c31775d7ca40f395a72564ca41a5d9fa9b6dcf0
superb1019 <superb1019@naver.com>
2020-01-29 14:01:24 +0900

    make RNN & CNN model from Open source

---
+++ code/2017803094_최지훈_HW2_MLP_test2.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+
+# MNIST 데이터 적재
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2' # 경고 메시지 화면출력 금지
+
+#파라미터
+learning_rate = 0.001 #신경망 학습률
+training_epochs = 100 # 학습 횟수 (epoch)
+batch_size = 100 # 미니배치의 크기
+display_step = 10 # 중간결과 출력 간격
+
+#신경망 구조 관련 파라미터
+n_hidden_1 = 15 # 은닉층의 노드 개수
+n_input = 784 #입력층의 노드 개수 MNIST 데이터 (28x28)
+n_classes = 10 # 출력층의 노드 수 MNIST 부류 개수(숫자 0~9)
+
+#텐서 그래프 입력 변수
+x = tf.placeholder("float", [None, n_input]) #입력 : 필기체 영상
+y = tf.placeholder("float", [None, n_classes]) #출력 : 숫자
+
+#학습모델 MLP정의
+def multilayer_perceptron(x, weights, biases):
+    #ReLu를 사용하는 은닉층
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.sigmoid(layer_1)
+    #출력층 (활성화 함수 미사용)
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    return out_layer
+
+#학습할 파라미터: 가중치(weights), 편차항(biases)
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'out': tf.Variable(tf.random_normal([n_hidden_1, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+#신경망 모델 구성, 출력값 pred : 입력 x에 대한 신경망의 출력
+pred = multilayer_perceptron(x, weights, biases)
+
+with tf.name_scope("cost"):
+    #비용(오차) 정의 (신경망 출력 pred, 목표 출력 y): 교차 엔트로피 사용
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    #학습 알고리즘 설정
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+    tf.summary.scalar('cost', cost)
+
+with tf.name_scope("accuracy"):
+    #모델 테스트 : out의 최대값 노드와 y 노드가 같으면 정답
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    #correct_prediction 평균
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    tf.summary.scalar('accuracy', accuracy)
+
+    
+init = tf.global_variables_initializer() #변수 초기화 지정
+
+#데이터 플로우 그래프 실행
+with tf.Session() as sess:
+    print("Start....")
+    writer = tf.summary.FileWriter("./logs/nn_logs", sess.graph)  # for 0.8
+    merged = tf.summary.merge_all()
+    
+    sess.run(init)
+    summary, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    total_batch = int(mnist.train.num_examples / batch_size) #배치 개수
+
+    for epoch in range(training_epochs): #정해진 횟수 만큼 학습
+        avg_cost = 0.
+        for i in range(total_batch): #미니 배치
+            batch_x, batch_y = mnist.train.next_batch(batch_size) #적재
+            #역전파 알고리즘 적용
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+            avg_cost += c / total_batch #평균 손실(오류) 계산
+
+            if epoch %display_step == 0: #현재 학습 상황 출력
+                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+            summary, acc = sess.run([merged, accuracy], feed_dict={x: mnist.test.images, y: mnist.test.labels})
+            writer.add_summary(summary, i)
+    
+    print(accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+    print("End...")
+    
+
+
+# In[ ]:
+
+
+
+
---
+++ code/2017803094_최지훈_HW2_test1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import timeit
+
+start = timeit.default_timer()
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True, reshape=False)
+
+X = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+Y_Label = tf.placeholder(tf.float32, shape=[None, 10])
+
+Kernel1 = tf.Variable(tf.truncated_normal(shape=[4, 4, 1, 4], stddev=0.1))
+Bias1 = tf.Variable(tf.truncated_normal(shape=[4], stddev=0.1))
+Conv1 = tf.nn.conv2d(X, Kernel1, strides=[1, 1, 1, 1], padding='SAME') + Bias1
+Activation1 = tf.nn.relu(Conv1)
+Pool1 = tf.nn.max_pool(Activation1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+Kernel2 = tf.Variable(tf.truncated_normal(shape=[4, 4, 4, 8], stddev=0.1))
+Bias2 = tf.Variable(tf.truncated_normal(shape=[8], stddev=0.1))
+Conv2 = tf.nn.conv2d(Pool1, Kernel2, strides=[1, 1, 1, 1], padding='SAME') + Bias2
+Activation2 = tf.nn.relu(Conv2)
+Pool2 = tf.nn.max_pool(Activation2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+W1 = tf.Variable(tf.truncated_normal(shape=[8 * 7 * 7, 10])) #8x7x7
+B1 = tf.Variable(tf.truncated_normal(shape=[10]))
+Pool2_flat = tf.reshape(Pool2, [-1, 8 * 7 * 7])
+OutputLayer = tf.matmul(Pool2_flat, W1) + B1
+
+with tf.name_scope("loss"):
+    Loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y_Label, logits=OutputLayer))
+    train_step = tf.train.AdamOptimizer(0.005).minimize(Loss)
+    tf.summary.scalar('loss', Loss)
+
+with tf.name_scope("accuracy"):
+    correct_prediction = tf.equal(tf.argmax(OutputLayer, 1), tf.argmax(Y_Label, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
+
+with tf.Session() as sess:
+    print("Start....")
+    writer = tf.summary.FileWriter("./logs/nn_logs", sess.graph)  # for 0.8
+    merged = tf.summary.merge_all()
+    
+    sess.run(tf.global_variables_initializer())
+    summary, acc = sess.run([merged, accuracy], feed_dict={X: mnist.test.images, Y_Label: mnist.test.labels})
+    for i in range(201):
+        trainingData, Y = mnist.train.next_batch(64)
+        sess.run(train_step,feed_dict={X:trainingData,Y_Label:Y})
+        if i%10 == 0 :
+            print(sess.run(accuracy, feed_dict={X:mnist.test.images, Y_Label:mnist.test.labels}))
+        summary, acc = sess.run([merged, accuracy], feed_dict={X: mnist.test.images, Y_Label: mnist.test.labels})
+        writer.add_summary(summary, i)
+        
+    end = timeit.default_timer()
+    print("End...")
+    print("Run Time : ", end - start)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
